542edc31923f1e9727dc51cd2cf2c72572f0175c
MrPutinx <barron_1998@hotmail.com>
2016-11-18 18:50:13 -0600

    Segunda modificacion PDV

--- PuntoDeVenta/src/puntodeventa/PuntoDeVenta.java
+++ PuntoDeVenta/src/puntodeventa/PuntoDeVenta.java
@@ -3,10 +3,14 @@ package puntodeventa;
 
 
 public class PuntoDeVenta {
-
+    String nombre;
+        int valor;
     
     public static void main(String[] args) {
         
+    }
+    public String getNombre(){
+        return nombre;
     }
     
 }
